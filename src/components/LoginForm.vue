7898ffa9515b58ece9ab74d4d0a7e21a38e31104
Kyle Waters <unum@unum5.org>
2018-11-14 18:03:55 +0000

    css update

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -39,7 +39,7 @@ export default {
       }
       localStorage.setItem('token', req.data.api_token)
       this.error = false
-      this.$router.push('/client');
+      this.$router.push('/clients');
     },
 
     loginFailed () {
@@ -52,7 +52,6 @@ export default {
 
 <style lang="css">
 body {
-  background: #605B56;
 }
 
 .login-wrapper {
