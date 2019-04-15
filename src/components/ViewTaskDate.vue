abc44d49f836b214d4b8421c71ccd4943500592d
Kyle Waters <unum@unum5.org>
2019-04-15 08:55:57 -0600

    fix formatting

--- src/components/ViewTaskDate.vue
+++ src/components/ViewTaskDate.vue
@@ -2,54 +2,54 @@
     <div>
         <b-container>
             <b-row>
-                <b-col>Project</b-col>
-                <b-col>{{ task_date.task.order.project.name }}</b-col>
-                <b-col>Client</b-col>
-                <b-col>{{ task_date.task.order.project.client.name }}</b-col>
-                <b-col>Order#</b-col>
-                <b-col>{{ task_date.task.order.id }}</b-col>
+                <b-col class="label">Project</b-col>
+                <b-col class="data">{{ task_date.task.order.project.name }}</b-col>
+                <b-col class="label">Client</b-col>
+                <b-col class="data">{{ task_date.task.order.project.client.name }}</b-col>
+                <b-col class="label">Order#</b-col>
+                <b-col class="data">{{ task_date.task.order.id }}</b-col>
             </b-row>
             <b-row>
-                <b-col>{{ task_date.task.order.project.notes }}</b-col>
+                <b-col class="data">{{ task_date.task.order.project.notes }}</b-col>
             </b-row>
             <b-row>
-                <b-col>Order</b-col>
-                <b-col>{{ task_date.task.order.name }}</b-col>
-                <b-col>Start Date</b-col>
-                <b-col>{{ task_date.task.order.start_date }}</b-col>
+                <b-col class="label">Order</b-col>
+                <b-col class="data">{{ task_date.task.order.name }}</b-col>
+                <b-col class="label">Start Date</b-col>
+                <b-col class="data">{{ task_date.task.order.start_date }}</b-col>
                 <b-col><b-button @click="signIn" v-if="!sign_in_id">Clock In</b-button></b-col>
                 <b-col><b-button @click="signOut" v-if="sign_in_id">Clock Out</b-button></b-col>
             </b-row>
             <b-row>
-                <b-col>{{ task_date.task.order.description }}</b-col>
+                <b-col class="data">{{ task_date.task.order.description }}</b-col>
             </b-row>
             <b-row>
                 <b-col>
                     <b-container>
                         <b-row>
-                            <b-col>Property</b-col>
-                            <b-col>
+                            <b-col class="label">Property</b-col>
+                            <b-col class="data">
                                 <a :href="'http://maps.google.com/maps?q=' + task_date.task.order.property.address1 + '+' + (task_date.task.order.property.address2 ? task_date.task.order.property.address2 + '+' : '') + task_date.task.order.property.city + '+' + task_date.task.order.property.state + '+' + task_date.task.order.property.zip" target="tasca_address">
                                     {{ task_date.task.order.property.name }}<br />
                                     {{ task_date.task.order.property.address1 }}<br />
                                     {{ task_date.task.order.property.address2 }}<br v-if="task_date.task.order.property.address2" />
                                     {{ task_date.task.order.property.city }},{{ task_date.task.order.property.state }} {{ task_date.task.order.property.zip }}
-                                    <b-col><a :href="'tel:' + task_date.task.order.property.phone_number">{{ task_date.task.order.property.phone_number }}</a></b-col>
+                                    <b-col class="data"><a :href="'tel:' + task_date.task.order.property.phone_number">{{ task_date.task.order.property.phone_number }}</a></b-col>
                                 </a>
                             </b-col>
                         </b-row>
                         <div v-for="contact in task_date.task.order.property.contacts" :key="contact.id">
                             <b-row>
-                                <b-col>Contact</b-col>
-                                <b-col>{{ contact.name }}</b-col>
+                                <b-col class="label">Contact</b-col>
+                                <b-col class="data">{{ contact.name }}</b-col>
                             </b-row>
                             <b-row v-for="phone_number in contact.phone_numbers" :key="phone_number.id">
-                                <b-col>{{ phone_number.phone_number_type.name }}</b-col>
-                                <b-col><a :href="'tel:' + phone_number.phone_number">{{ phone_number.phone_number }}</a></b-col>
+                                <b-col class="label">{{ phone_number.phone_number_type.name }}</b-col>
+                                <b-col class="data"><a :href="'tel:' + phone_number.phone_number">{{ phone_number.phone_number }}</a></b-col>
                             </b-row>
                             <b-row v-for="email in contact.emails" :key="email.id">
-                                <b-col>{{ email.email_type.name }}</b-col>
-                                <b-col><a :href="'mailto:' + email.email">{{ email.email }}</a></b-col>
+                                <b-col class="label">{{ email.email_type.name }}</b-col>
+                                <b-col class="data"><a :href="'mailto:' + email.email">{{ email.email }}</a></b-col>
                             </b-row>
                         </div>
                     </b-container>
@@ -57,65 +57,65 @@
                 <b-col>
                     <b-container>
                         <b-row>
-                            <b-col>Location</b-col>
-                            <b-col>{{ task_date.task.order.location }}</b-col>
+                            <b-col class="label">Location</b-col>
+                            <b-col class="data">{{ task_date.task.order.location }}</b-col>
                         </b-row>
                         <b-row>
-                            <b-col>Instructions</b-col>
-                            <b-col>{{ task_date.task.order.instructions }}</b-col>
+                            <b-col class="label">Instructions</b-col>
+                            <b-col class="data">{{ task_date.task.order.instructions }}</b-col>
                         </b-row>
                         <b-row>
-                            <b-col>Notes</b-col>
-                            <b-col>{{ task_date.task.order.notes }}</b-col>
+                            <b-col class="label">Notes</b-col>
+                            <b-col class="data">{{ task_date.task.order.notes }}</b-col>
                         </b-row>
                         <b-row>
-                            <b-col>Budget</b-col>
-                            <b-col>{{ task_date.task.order.budget }}+/-{{ task_date.task.order.budget_plus_minus }}</b-col>
+                            <b-col class="label">Budget</b-col>
+                            <b-col class="data">{{ task_date.task.order.budget }}+/-{{ task_date.task.order.budget_plus_minus }}</b-col>
                         </b-row>
                         <b-row>
-                            <b-col>Bid</b-col>
-                            <b-col>{{ task_date.task.order.bid }}+/-{{ task_date.task.order.bid_plus_minus }}</b-col>
+                            <b-col class="label">Bid</b-col>
+                            <b-col class="data">{{ task_date.task.order.bid }}+/-{{ task_date.task.order.bid_plus_minus }}</b-col>
                         </b-row>
                     </b-container>
                 </b-col>
             </b-row>
             <b-row>
-                <b-col>Task</b-col>
-                <b-col>{{ task_date.task.name }}</b-col>
-                <b-col>Hours</b-col>
-                <b-col>{{ task_date.task.task_hours }}</b-col>
-                <b-col>Crew Time</b-col>
-                <b-col>{{ task_date.task.crew_hours }}</b-col>
+                <b-col class="label">Task</b-col>
+                <b-col class="data">{{ task_date.task.name }}</b-col>
+                <b-col class="label">Hours</b-col>
+                <b-col class="data">{{ task_date.task.task_hours }}</b-col>
+                <b-col class="label">Crew Time</b-col>
+                <b-col class="data">{{ task_date.task.crew_hours }}</b-col>
             </b-row>
             <b-row>
-                <b-col>Schedule Date & Time</b-col>
-                <b-col>{{ task_date.date }} {{ task_date.time }}</b-col>
+                <b-col class="data">{{ task_date.task.description }}</b-col>
             </b-row>
             <b-row>
-                <b-col>Day Notes</b-col>
-                <b-col>{{ task_date.notes }}</b-col>
+                <b-col class="label">Schedule Date & Time</b-col>
+                <b-col class="data">{{ task_date.date }} {{ task_date.time }}</b-col>
             </b-row>
             <b-row>
-                <b-col>{{ task_date.task.description }}</b-col>
+                <b-col class="label">Day Notes</b-col>
+                <b-col class="data">{{ task_date.notes }}</b-col>
             </b-row>
             <b-row>
-                <b-col>Completed</b-col>
+                <b-col class="label">Completed</b-col>
                 <b-col>
                     <b-form-checkbox v-model="completed" @input="markCompleted">
                     </b-form-checkbox>
                 </b-col>
-                <b-col>Billed</b-col>
+                <b-col class="label">Billed</b-col>
                 <b-col>
                     <b-form-checkbox v-model="billed" @input="markBilled">
                     </b-form-checkbox>
                 </b-col>
             </b-row>
             <b-row>
-                <b-col>Employee</b-col>
-                <b-col>Sign In</b-col>
-                <b-col>Sign Out</b-col>
-                <b-col>Hours</b-col>
-                <b-col>Labor Category</b-col>
+                <b-col class="header">Employee</b-col>
+                <b-col class="header">Sign In</b-col>
+                <b-col class="header">Sign Out</b-col>
+                <b-col class="header">Hours</b-col>
+                <b-col class="header">Labor Category</b-col>
             </b-row>
             <div v-for="sign_in in sign_ins" :key="sign_in.id">
                 <b-row>
@@ -126,8 +126,8 @@
                     <b-col></b-col>
                 </b-row>
                 <b-row>
-                    <b-col>Notes For The Day</b-col>
-                    <b-col><b-form-input v-model="sign_in.notes" @input="saveNotes(sign_in)"></b-form-input></b-col>
+                    <b-col class="label">Notes For The Day</b-col>
+                    <b-col class="data"><b-form-input v-model="sign_in.notes" @input="saveNotes(sign_in)"></b-form-input></b-col>
                 </b-row>
             </div>
         </b-container>
@@ -246,3 +246,17 @@ export default {
     }
 }
 </script>
+<style scoped>
+.label {
+    text-align: right;
+    font-weight: bold;
+}
+
+.header {
+    font-weight: bold;
+}
+
+.data {
+    text-align: left;
+}
+</style>
