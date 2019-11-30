b7e9ebf60dbd6f11f1ab51a418dad2eaa0d08909
Kyle Waters <unum@unum5.org>
2019-11-29 18:11:45 -0700

    show better reports results

--- src/components/ViewBackflowTestReports.vue
+++ src/components/ViewBackflowTestReports.vue
@@ -30,6 +30,22 @@
                 <template v-slot:cell(id)="data">
                     <a :href="'/backflow_test_report/' + data.value"> {{ data.value }} </a>
                 </template>
+                
+                
+                <template v-slot:cell(result)="data">
+                    <div v-if="['DCDA', 'DC'].includes(data.item.backflow_assembly.backflow_type.name)">
+                        <b-badge v-if="data.item.last_test.reading_1 >= 1 && data.item.last_test.reading_2 >= 1" variant="success">Closed Tight</b-badge>
+                        <b-badge v-else variant="danger">Leaked</b-badge>
+                    </div>
+                    <div v-if="['PCVB', 'AVB', 'SVB', 'PVB'].includes(data.item.backflow_assembly.backflow_type.name)">
+                        <b-badge v-if="(data.item.last_test.reading_1 >= 1) && (data.item.last_test.reading_2) >= 1" variant="success">Opened Under 1#</b-badge>
+                        <b-badge v-else variant="danger">Did Not Open</b-badge>
+                    </div>
+                    <div v-if="['RPDA', 'RP'].includes(data.item.backflow_assembly.backflow_type.name)">
+                        <b-badge v-if="data.item.last_test.reading_1 - data.item.last_test.reading_2 >= 2" variant="success">Closed Tight</b-badge>
+                        <b-badge v-else variant="danger">Leaked</b-badge>
+                    </div>
+                </template>
             </b-table>
         </main>
     </div>
@@ -72,21 +88,44 @@ export default {
                         sortable: true
                     },
                     {
-                        key: 'created_at',
-                        label: 'Created At',
+                        key: 'last_test.tested_on',
+                        label: 'Date',
                         sortable: true
                     },
                     {
-                        key: 'updated_at',
-                        label: 'Updated At',
+                        key: 'backflow_assembly.backflow_type.name',
+                        label: 'Type',
                         sortable: true
                     },
+                    {
+                        key: 'last_test.reading_1',
+                        label: 'Reading 1',
+                        sortable: true
+                    },
+                    {
+                        key: 'last_test.reading_2',
+                        label: 'Reading 2',
+                        sortable: true
+                    },
+                    {
+                        key: 'result',
+                        label: 'Result',
+                        sortable: false
+                    }
             ]
         }
     },
     created() {
-        this.$http.get('/backflow_test_reports?includes=backflow_assembly').then(response => {
+        this.$http.get('/backflow_test_reports?includes=backflow_assembly,backflow_tests,backflow_assembly.backflow_type').then(response => {
             this.backflow_test_reports = response.data.data;
+            this.backflow_test_reports.map(r => {
+                if(r.backflow_tests.length > 0){
+                    r.last_test = r.backflow_tests[r.backflow_tests.length-1];
+                }
+                else{
+                    r.last_test = {};
+                }
+            });
         });
     }
 }
