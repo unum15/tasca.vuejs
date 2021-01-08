33f660e93051d83274ea404203b4a3c1a76ffed7
Kyle Waters <unum@unum5.org>
2021-01-08 06:24:11 -0700

    add privacy

---
+++ src/components/Privacy.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Your data is yours.  We have no interest in it and will not share it with anyone.  The only reason we will look at your data is for quality assurance purposes.
+    </div>
+</template>
+<script>
+export default {
+    name: 'Privacy',
+    props: {
+    },
+    data () {
+        return {
+        };
+    },
+    created () {
+    },
+    methods: {
+    },
+    computed:{
+    },
+};
+</script>
+
