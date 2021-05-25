83ff2c8f737644d8ab70362b89e41930ac8f24aa
Kyle Waters <unum@unum5.org>
2021-05-25 10:29:58 -0600

    restore header filters

--- src/components/ViewScheduleOrdersTab.vue
+++ src/components/ViewScheduleOrdersTab.vue
@@ -28,7 +28,7 @@
                 primary-key="id"
                 style="text-align:left;"
                 >
-                 <template #thead-top="data">
+                 <template #thead-top>
                     <tr>
                         <th v-for="field in fields" :key="field.key">
                             <b-form-input type="text" @input="filterColumns" v-model="field.filter" v-if="field.filter !== undefined "></b-form-input>
